1896a922f9354a305d223fa9b9fbeb7d4d493228
Kevin Uriel Domínguez Riquelme <backtunn@gmail.com>
2019-07-10 18:05:24 -0500

    Tema 7, ejercicios, se agrega API de flickr

--- TiendaImagina/wwwroot/js/site.js
+++ TiendaImagina/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $("#botonfli").click(function () {
+        $("#imagenes").empty();
+        $("<img>").attr("src", "ajax-loader.gif").appendTo("#imagenes");
+        var valor = $("#selector").val();
+        var url = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
+        url += valor + "&format=json&jsoncallback=?";
+        $.getJSON(url, function (data, st, xhr) {
+            var arrFotos = data.items;
+            if (st = "success") {
+                $("#imagenes").empty();
+                $.each(arrFotos, function (i, item) {
+                    if (i < 10) {
+                        var img = "<img src='" + item.media.m + "' width='300' height='200'>";
+                        $("#imagenes").append(img);
+                    }
+                });
+            }
+        });
+    });
+});
