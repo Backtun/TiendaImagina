7c704ed6ff78c997abe92cc421edc83b77655126
Kevin Uriel Domínguez Riquelme <backtunn@gmail.com>
2019-07-16 18:26:10 -0500

    Se finaliza el tema 8 y sus ejercicios.

---
+++ TiendaImagina/Models/BDInicial.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace TiendaImagina.Models
+{
+    public class BDInicial
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TiendaImaginaContext(
+                serviceProvider.GetRequiredService<DbContextOptions<TiendaImaginaContext>>()))
+            {
+                // Busca si existen Productos.
+                if (context.Producto.Any())
+                {
+                    //En caso de que hayan productos en la base de datos no hará la inicialización.
+                    return;
+                }
+
+                //Añadimos productos    
+                context.Producto.AddRange(
+                     new Producto
+                     {
+                         Nombre = "Unity",
+                         Precio = 1000,
+                         Fecha = DateTime.Now,
+                         CategoriaId = 2
+                     },
+
+                     new Producto
+                     {
+                         Nombre = ".NET",
+                         Precio = 500,
+                         Fecha = DateTime.Now,
+                         CategoriaId = 2
+                     }
+                );
+
+                context.Categoria.AddRange(
+                new Categoria
+                {
+                    Nombre = "Python",
+                    Fecha = DateTime.Now
+                }
+
+                );
+
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- TiendaImagina/Models/Producto.cs
+++ TiendaImagina/Models/Producto.cs
@@ -4,14 +4,24 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TiendaImagina.Models
 {
     public class Producto
     {
         public long ProductoId { get; set; }
+
+        [StringLength(40, MinimumLength = 4)]
+        [Required]
         public string Nombre { get; set; }
+
+        [Range(1, 1000)]
+        [BindRequired]
         public decimal Precio { get; set; }
+
+        [Range(typeof(DateTime), "1/1/2016", "1/1/2020")]
         public DateTime Fecha { get; set; }
 
         [DisplayName("Categoria")]
